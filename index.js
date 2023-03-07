ae0ebc81f0fbb24c6e037bece06e516024ff8ff6
Abdurahim Miftah <abdugreat3@gmail.com>
2023-03-07 11:45:54 +0200

    Fix linter errors

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function openPopUp() {
-  document.querySelector(".popup-nav").classList.add("popup-nav-visible")
+  document.querySelector('.popup-nav').classList.add('popup-nav-visible');
 }
 
 function closePopUp() {
-  document.querySelector(".popup-nav").classList.replace("popup-nav-visible","popup-nav")
+  document.querySelector('.popup-nav').classList.replace('popup-nav-visible', 'popup-nav');
 }
 
-document.querySelector(".menu_icon").addEventListener("click",openPopUp)
-document.querySelector(".cancel-button").addEventListener("click",closePopUp)
-document.querySelector(".popup-menu .menu-item").addEventListener("click",closePopUp)
-document.querySelector(".popup-menu .menu-item2").addEventListener("click",closePopUp)
-document.querySelector(".popup-menu .menu-item3").addEventListener("click",closePopUp)
-document.querySelector(".popup-nav").addEventListener("click",closePopUp)
+document.querySelector('.menu_icon').addEventListener('click', openPopUp);
+document.querySelector('.cancel-button').addEventListener('click', closePopUp);
+document.querySelector('.popup-menu .menu-item').addEventListener('click', closePopUp);
+document.querySelector('.popup-menu .menu-item2').addEventListener('click', closePopUp);
+document.querySelector('.popup-menu .menu-item3').addEventListener('click', closePopUp);
+document.querySelector('.popup-nav').addEventListener('click', closePopUp);
